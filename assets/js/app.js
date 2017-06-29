ae3ce075638197fecc5252d8d2f121c71e7785d3
James Winkle <jwin4740@gmail.com>
2017-06-29 12:58:34 -0400

    data ordered and correct

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,13 @@
-// changed JSON order to put the top earnings on top
-// could have sorted the data if I didn't have physical access to the JSON
+
 
 var jsonData = [{
-        "name": "URBank",
-        "apy": 0.87,
-        "earnings": 436.89
-    }, {
         "name": "Big Guy Financial",
         "apy": 0.75,
         "earnings": 376.41
+    }, {
+        "name": "URBank",
+        "apy": 0.87,
+        "earnings": 436.89
     },
     {
         "name": "Dewey, Cheatam & Howe",
@@ -27,21 +26,30 @@ var jsonData = [{
     }
 ];
 var dataLength = jsonData.length;
-console.log(jsonData[0]["name"]);
-console.log(jsonData.length);
-// construct table row
+
+// sorts data by apy
+function sortData(prop) {
+    return function (a, b) {
+        if (a[prop] < b[prop]) {
+            return 1;
+        } else if (a[prop] > b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
+
+jsonData.sort(sortData("apy"));
 
 for (var i = 0; i < dataLength; i++) {
-
     var novoRow = $("<tr class='tRow'>");
     var novoColumnBank = $("<td class='bankName'>");
     novoColumnBank.text(jsonData[i]["name"]);
     var novoColumnapy = $("<td class='apy'>");
-    novoColumnapy.text(jsonData[i]["apy"]);
+    novoColumnapy.text(jsonData[i]["apy"] + " %");
     var novoColumnEarnings = $("<td class='earnings'>");
-    novoColumnEarnings.text(jsonData[i]["earnings"]);
+    novoColumnEarnings.text("$" + (jsonData[i]["earnings"]).toFixed(2));
     novoRow.append(novoColumnBank).append(novoColumnapy).append(novoColumnEarnings);
-
 
     $("tbody").append(novoRow);
 }
